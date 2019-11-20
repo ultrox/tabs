1357c0235d454ab58cec54d59a1d88683f3713a4
Marko <crashxx@gmail.com>
2019-11-20 16:27:25 +0100

    Add Tabs initial logic
    This is proof of concept, tabs very fragile.

--- src/Tabs.js
+++ src/Tabs.js
@@ -2,14 +2,21 @@ import React from 'react'
 
 // tabs will have all the state, like active index
 export function Tabs(props) {
-  return <div className="Tabs">{props.children}</div>
-}
+  const [index, setIndex] = React.useState(0)
 
+  const newChildren = React.Children.map(props.children, child => {
+    return React.cloneElement(child, {
+      activeIndex: index,
+      onSelectTab: setIndex,
+    })
+  })
+  return <div className="Tabs">{newChildren}</div>
+}
 
 // responsible for rendering specific tab (will receive array of TabPanel)
 export function TabPanels(props) {
-  const activeIndex = 0 
-  return <div className="panels">{props.children}</div>
+  const { activeIndex } = props
+  return <div className="panels">{props.children[activeIndex]}</div>
 }
 
 // TabList will receive array of Tab, from `Tabs`
@@ -19,12 +26,23 @@ export function TabPanel(props) {
 
 // children are arr of Tab
 export function TabList(props) {
-  return <div className="tabs">{props.children}</div>
+  const newChildren = React.Children.map(props.children, (child, i) => {
+    return React.cloneElement(child, {
+      onSelect: _ => props.onSelectTab(i),
+      isActive: props.activeIndex === i,
+    })
+  })
+
+  return <div className="tabs">{newChildren}</div>
 }
 
 // Tab need to activate new active index
 export function Tab(props) {
-  return <div className="tab">{props.children}</div>
+  return (
+    <div
+      onClick={props.onSelect}
+      className={props.isActive ? 'active tab' : 'tab'}>
+      {props.children}
+    </div>
+  )
 }
-
-
